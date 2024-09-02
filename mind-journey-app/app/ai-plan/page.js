2f7339238121d37fa017dfe2e257c89402b85c4c
ryan <concepting@protonmail.com>
2024-09-02 00:14:38 -0400

    card stuff

--- mind-journey-app/app/ai-plan/page.js
+++ mind-journey-app/app/ai-plan/page.js
@@ -8,11 +8,13 @@ import '../other.css';
 import { db } from '@/firebase'
 import { doc, collection, setDoc, getDoc, writeBatch, memoryEagerGarbageCollector } from 'firebase/firestore'
 
-import { Container, Grid, AppBar, Toolbar, Box, Button, Stack, Typography, TextField, Card } from '@mui/material'
+import { Container, Grid, AppBar, Toolbar, Box, Button, Stack, Typography, TextField } from '@mui/material'
 import { SignedIn, SignedOut, isSignedIn, user, useUser, UserButton } from "@clerk/nextjs";
 
 import { createTheme } from '@mui/material/styles';
 import InputAdornment from '@mui/material/InputAdornment';
+import { AnimatePresence, motion } from "framer-motion";
+import { CanvasRevealEffect } from "@/components/ui/canvas-reveal-effect";
 
 import { Jost } from "next/font/google";
 import Link from "next/link";
@@ -50,6 +52,85 @@ const theme = createTheme({
     },
   });
 const sendIcon = '/images/send.png';
+
+const Card = ({
+  title,
+  icon,
+  children,
+}) => {
+  const [hovered, setHovered] = React.useState(false);
+  return (
+    <div
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
+      className="border border-black/[0.2] group/canvas-card flex items-center justify-center dark:border-white/[0.2]  max-w-sm w-full mx-auto p-4 relative h-[30rem] relative"
+    >
+      <Icon className="absolute h-6 w-6 -top-3 -left-3 dark:text-white text-black" />
+      <Icon className="absolute h-6 w-6 -bottom-3 -left-3 dark:text-white text-black" />
+      <Icon className="absolute h-6 w-6 -top-3 -right-3 dark:text-white text-black" />
+      <Icon className="absolute h-6 w-6 -bottom-3 -right-3 dark:text-white text-black" />
+ 
+      <AnimatePresence>
+        {hovered && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            className="h-full w-full absolute inset-0"
+          >
+            {children}
+          </motion.div>
+        )}
+      </AnimatePresence>
+ 
+      <div className="relative z-20">
+        <div className="text-center group-hover/canvas-card:-translate-y-4 group-hover/canvas-card:opacity-0 transition duration-200 w-full  mx-auto flex items-center justify-center">
+          {icon}
+        </div>
+        <h2 className="dark:text-white text-xl opacity-0 group-hover/canvas-card:opacity-100 relative z-10 text-black mt-4  font-bold group-hover/canvas-card:text-white group-hover/canvas-card:-translate-y-2 transition duration-200">
+          {title}
+        </h2>
+      </div>
+    </div>
+  );
+};
+ 
+const AceternityIcon = () => {
+  return (
+    <svg
+      width="66"
+      height="65"
+      viewBox="0 0 66 65"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className="h-10 w-10 text-black dark:text-white group-hover/canvas-card:text-white "
+    >
+      <path
+        d="M8 8.05571C8 8.05571 54.9009 18.1782 57.8687 30.062C60.8365 41.9458 9.05432 57.4696 9.05432 57.4696"
+        stroke="currentColor"
+        strokeWidth="15"
+        strokeMiterlimit="3.86874"
+        strokeLinecap="round"
+        style={{ mixBlendMode: "darken" }}
+      />
+    </svg>
+  );
+};
+ 
+export const Icon = ({ className, ...rest }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth="1.5"
+      stroke="currentColor"
+      className={className}
+      {...rest}
+    >
+      <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v12m6-6H6" />
+    </svg>
+  );
+};
 
 export default function Plan() {
   const { isLoading, isSignedIn, user } = useUser();
@@ -241,6 +322,7 @@ export default function Plan() {
         sx = {{
           boxShadow: 3,
           borderRadius: 4,
+          backgroundColor: theme.palette.primary.contrastText,
         }}
           direction={'column'}
           width="80vw"
@@ -278,6 +360,7 @@ export default function Plan() {
                   sx={{
                     borderRadius: 6,
                     color: theme.palette.secondary.contrastText,
+                    
                   }}
                   color="white"
                   p={2}
@@ -359,9 +442,36 @@ export default function Plan() {
           justifyContent="center"
           alignItems="center"
         >
-          <Card variant="h5" sx={{color: theme.palette.primary.contrastText, margin: 10}} className={jost.className}>Card 1</Card>
-          <Card variant="h5" sx={{color: theme.palette.primary.contrastText, margin: 10}} className={jost.className}>Card 2</Card>
-          <Card variant="h5" sx={{color: theme.palette.primary.contrastText, margin: 10}} className={jost.className}>Card 3</Card>
+          <div className="py-20 flex flex-col lg:flex-row items-center justify-center bg-white dark:bg-white w-full gap-4 mx-auto px-8 rounded-md">
+            <Card title="Sheetal is Nisha" icon={<AceternityIcon />}>
+              <CanvasRevealEffect
+                animationSpeed={2.5}
+                containerClassName="bg-emerald-900"
+                dotSize={1}
+              />
+            </Card>
+            <Card title="Nisha is Munni" icon={<AceternityIcon />}>
+              <CanvasRevealEffect
+                animationSpeed={2.5}
+                containerClassName="bg-zinc-900"
+                colors={[
+                  [236, 72, 153],
+                  [232, 121, 249],
+                ]}
+                dotSize={2}
+              />
+              {/* Radial gradient for the cute fade */}
+              <div className="absolute inset-0 [mask-image:radial-gradient(400px_at_center,white,transparent)] bg-black/50 dark:bg-black/90" />
+            </Card>
+            <Card title="Munni is Aditi" icon={<AceternityIcon />}>
+              <CanvasRevealEffect
+                animationSpeed={2.5}
+                containerClassName="bg-sky-600"
+                colors={[[125, 211, 252]]}
+                dotSize={3}
+              />
+            </Card>
+          </div>
         </Box>
       </Grid>
     </Box>
